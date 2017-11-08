3a881f905107488e3952298a6eeb91e746e15830
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-11-08 22:18:49 +0100

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+
+    // Deklarer og initializerer variablerne
+    String name = "";
+    String address = "";
+    int phoneNumber = 0;
+    String e_mail_address = "";
+
+}
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student extends Person {
+
+    // Laver et String array med 4 statuser som er constante
+    static final String[] STATUS = {"freshman", "sophomore", "junior", "senior"};
+
+
+
+}
